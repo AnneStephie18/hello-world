64479df1e225148c804c48e5aeedb5145f38f96b
anne.sivakumar <anne.sivakumar@atmecs.com>
2019-06-10 17:30:14 +0530

    anne first commit

---
+++ NewProject/src/Program1.java
@@ -0,0 +1,16 @@
+
+
+
+public class Program1 {
+	public static void main(String[] args) {
+		Program1 a=new Program1();
+		a.method();
+		
+	}
+	public void method()
+	{
+	System.out.println("my program");
+	}
+	}
+
+
